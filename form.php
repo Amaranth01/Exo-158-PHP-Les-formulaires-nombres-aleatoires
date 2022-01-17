76f7bf5059e5e3634b8c069d9dbeea2e337e82bc
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-17 17:41:53 +0100

    Exo 158 ok

--- form.php
+++ form.php
@@ -1,8 +1,11 @@
 <?php
 
 $x = $_POST["number1"];
-
 $y = $_POST["number2"];
+
+if (!is_numeric($x) || !is_numeric($y) || $y < $x || $y > getrandmax()){
+    header("Location: index.php?error=1");
+}
 
 $random = rand($x, $y);
 echo "Votre nombre est : " . $random;
